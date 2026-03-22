294174f92551738121eee90b1440e65bef3ca0e2
Endalk Gizaw <end.gizaw@gmail.com>
2026-03-22 08:28:32 -0700

    ArrayStack draft

--- ArrayStack.h
+++ ArrayStack.h
@@ -4,33 +4,54 @@
 #include <vector>
 #include <stdexcept>
 
+using namespace std;
+
+
 template <typename T>
 class ArrayStack {
 private:
-    std::vector<T> data;
+    vector<T> data;
 
 public:
     void push(const T& value) {
+        if (ifFull(data)) {
+            return;
+        } else {
+            data.push_back(value);
+        }
         // TODO
     }
 
     void pop() {
+        if (isEmpty(data)) {
+            cout << "Is Empty!" ;
+            return;
+        } else {
+            data.pop_back();
+        }
         // TODO
     }
 
     T top() const {
+        if (isEmpty(data)) {
+            cout << "Is Empty!" ;
+            return T();
+        }
         // TODO
         return T();
     }
 
     bool empty() const {
+        if (!isEmpty(data)) {
+            return false;
+        }
         // TODO
         return true;
     }
 
     int size() const {
         // TODO
-        return 0;
+        return data.size();;
     }
 };
 
